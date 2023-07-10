bf9cfb136f22ba6b0627a8ceb35b79c3c89ff8ea
Humaira Hossain Methela <hhm639069@gmail.com>
2023-07-10 16:09:10 +0600

    section added

--- common/js/script.js
+++ common/js/script.js
@@ -26,3 +26,26 @@ onscroll = () => {
     window.scrollTo(0, 0);
     setTimeout(function(){ document.getElementById('gototop').classList.add('d-none') }, 300);
   }
+//sticky//
+
+// Step 1: Retrieve the element to make sticky
+const stickyElement = document.getElementById('stickyElement');
+
+// Step 2: Determine the desired scroll position for stickiness
+const desiredPosition = 200;
+
+// Step 3: Add event listener to the scroll event
+window.addEventListener('scroll', handleScroll);
+
+// Step 4: Event handler for scroll event
+function handleScroll() {
+  // Step 5: Get the current scroll position
+  const scrollPosition = window.pageYOffset || window.scrollY;
+
+  // Step 6: Add or remove CSS class based on scroll position
+  if (scrollPosition >= desiredPosition) {
+    stickyElement.classList.add('sticky');
+  } else {
+    stickyElement.classList.remove('sticky');
+  }
+}
