b2f5d34963abab840cb2b9f66c4caab36011666f
MomotazRahman <momo29intk@gmail.com>
2023-07-13 17:49:48 +0600

    added

--- common/js/script.js
+++ common/js/script.js
@@ -2,30 +2,57 @@
 
 
 
-const exitButton= document.querySelector('.exit-button');
+const exitButton = document.querySelector('.exit-button');
 
-exitButton.addEventListener('click',function(){
+exitButton.addEventListener('click', function () {
 
-    const photoContainer=this.parentNode;
-    photoContainer.remove();
+  const photoContainer = this.parentNode;
+  photoContainer.remove();
 
 });
 
 
 ///goto button///
 
+// onscroll = () => {
+//   if (document.documentElement.scrollTop > 500) {
+//     setTimeout(function () { document.getElementById('gototop').classList.remove('d-none') }, 300);
+//   } else {
+//     setTimeout(function () { document.getElementById('gototop').classList.add('d-none') }, 300);
+//   }
+// };
+
+// function gototop() {
+//   window.scrollTo(0, 0);
+//   setTimeout(function () { document.getElementById('gototop').classList.add('d-none') }, 300);
+//   gototop.addEventListener('click', function () {
+//     window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth'
+//     });
+//   });
+// }
+
+
 onscroll = () => {
-    if(document.documentElement.scrollTop > 500){
-      setTimeout(function(){ document.getElementById('gototop').classList.remove('d-none') }, 300);
-    }else{
-      setTimeout(function(){ document.getElementById('gototop').classList.add('d-none') }, 300);
-    }
-  };
-  
-  function gototop () {
-    window.scrollTo(0, 0);
-    setTimeout(function(){ document.getElementById('gototop').classList.add('d-none') }, 300);
+  if (document.documentElement.scrollTop > 500) {
+    setTimeout(function () { document.getElementById('gototop').classList.remove('d-none') }, 300);
+  } else {
+    setTimeout(function () { document.getElementById('gototop').classList.add('d-none') }, 300);
   }
+};
+
+function gototop() {
+  window.scrollTo(0, 0);
+  setTimeout(function () { document.getElementById('gototop').classList.add('d-none') }, 500);
+}
+
+
+
+
+
+
+
 //sticky//
 
 const stickyElement = document.getElementById('stickyElement');
